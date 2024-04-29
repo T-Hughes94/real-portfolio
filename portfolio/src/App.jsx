5cac6d04c78d8010dbfe7cb12d54460cd36fb362
Terence Hughes <Terencehughes450@gmail.com>
2024-04-29 06:31:19 -0400

    added aioutlinemenu from react-icons and deleted stock css file also removed css import from app.jsx file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import Sidenav from './components/Sidenav'
 
 function App() {
 
   return (
     <>
      <div>
-      
+      <Sidenav/>
      </div>
     </>
   )
