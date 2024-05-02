d546354901e41d7c2523cfd682784ac3b0c80aef
Terence Hughes <Terencehughes450@gmail.com>
2024-05-02 02:33:02 -0400

    added logo photos and links for all language buttons on the technicalSkills component

--- portfolio/src/components/TechnicalSkills.jsx
+++ portfolio/src/components/TechnicalSkills.jsx
@@ -3,6 +3,12 @@ import jsLogo from '../assets/js-logo.png'; // Adjust the path to your language 
 import pythonLogo from '../assets/python-logo.png';
 import reactLogo from '../assets/react-logo.png';
 import sqlLogo from '../assets/sql-logo.png';
+import htmlLogo from '../assets/html-logo.png';
+import cssLogo from '../assets/css-logo.png';
+import sqlaLogo from '../assets/sqla-logo.png';
+import tlwndLogo from '../assets/tlwnd-logo.png';
+import flaskLogo from '../assets/flask-logo.png';
+
 
 const TechnicalSkills = () => {
   return (
@@ -50,6 +56,7 @@ const TechnicalSkills = () => {
             Learn React
           </a>
         </div>
+        {/* SQL */}
         <div className="flex flex-col items-center">
           <img src={sqlLogo} alt="Sql" className="w-16 h-16" />
           <a
@@ -61,6 +68,67 @@ const TechnicalSkills = () => {
             Learn SQL
           </a>
         </div>
+        {/* HTML */}
+        <div className="flex flex-col items-center">
+          <img src={htmlLogo} alt="html" className="w-16 h-16" />
+          <a
+            href="https://www.freecodecamp.org/news/sql-and-databases-full-course/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Learn HTML
+          </a>
+        </div>
+        {/* CSS */}
+        <div className="flex flex-col items-center">
+          <img src={cssLogo} alt="html" className="w-16 h-16" />
+          <a
+            href="https://www.freecodecamp.org/news/sql-and-databases-full-course/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Learn CSS
+          </a>
+        </div>
+        {/* SQLalchemy */}
+        <div className="flex flex-col items-center">
+          <img src={sqlaLogo} alt="html" className="w-16 h-16" />
+          <a
+            href="https://www.sqlalchemy.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Learn SQLalchemy
+          </a>
+        </div>
+        {/* Tailwind */}
+        <div className="flex flex-col items-center">
+          <img src={tlwndLogo} alt="html" className="w-16 h-16" />
+          <a
+            href="https://www.sqlalchemy.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Learn Tailwind
+          </a>
+        </div>
+        {/* Flask */}
+        <div className="flex flex-col items-center">
+          <img src={flaskLogo} alt="html" className="w-16 h-16" />
+          <a
+            href="https://flask.palletsprojects.com/en/3.0.x/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+          >
+            Learn FLask
+          </a>
+        </div>
+        
       </div>
     </div>
   );
